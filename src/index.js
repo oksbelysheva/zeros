44622bae965ff34f3624d68bbe6a46c2d85d2890
oksbelysheva <oks.belysheva@yandex.ru>
2019-09-24 10:49:10 +0400

    feat: add task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function zeros(expression) {
-  // your solution
+  let arrayFactorials = expression.split('*'),
+      countFive = 0,
+      countTwo = 0;
+  arrayFactorials.forEach(element => {
+    let number = Number.parseInt(element),
+        even = 1,
+        add = 1;
+    element.includes('!!') && (even = (number % 2) == 0 ? 2 : 1) && (add = 2);
+    for(let i = even; i <= number; i+=add){
+      countFive += countOfDrivers(i, 5);
+      countTwo += countOfDrivers(i, 2);
+    }  
+  });
+  return countFive <= countTwo ? countFive : countTwo;  
+}
+
+function countOfDrivers(number, driver){
+  let count = 0;
+  while(number % driver == 0){
+    number /= driver;
+    count++;
+  }
+  return count;
 }
